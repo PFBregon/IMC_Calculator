4ddbe08efbb296ba85bec2fb6aa455bdc3b9b0af
PFBregon <bregon@gmail.com>
2025-03-19 15:06:03 +0100

    testCalcularIMC exception included and run ok

--- src/test/java/dev/patriciafb/imc/Test/ImcCalculatorTest.java
+++ src/test/java/dev/patriciafb/imc/Test/ImcCalculatorTest.java
@@ -13,6 +13,12 @@ private final ImcCalculator calculator = new ImcCalculator();
         int peso = 70;
         double altura = 1.75;
         double imc = calculator.calcularIMC(peso, altura);
-        assertEquals(22.86, imc);
+        assertEquals(22.86, imc, 0.01);
+    }
+
+       public double calcularIMC(int peso, double altura) {
+        if (altura <= 0) {
+            throw new IllegalArgumentException("La altura debe ser mayor que 0");
+        }return peso / (altura * altura);
     }
 }
