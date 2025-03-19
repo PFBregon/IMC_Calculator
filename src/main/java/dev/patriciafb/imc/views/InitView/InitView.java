2f8bfdea97e34d80610e86a60744d99dc6f7cba6
PFBregon <bregon@gmail.com>
2025-03-19 15:55:25 +0100

    InitViewTest run ok

--- src/main/java/dev/patriciafb/imc/views/InitView/InitView.java
+++ src/main/java/dev/patriciafb/imc/views/InitView/InitView.java
@@ -1,16 +1,25 @@
 package dev.patriciafb.imc.views.InitView;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class InitView {
+    private final Scanner scanner;
 
     public InitView(Scanner scanner) {
-        //TODO Auto-generated constructor stub
+        this.scanner = scanner;
+        this.scanner.useLocale(Locale.US);
     }
 
     public void printInitMessage() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'printInitMessage'");
+        System.out.println("Bienvenido. Ahora puedes calcular tu IMC.");
+        System.out.println("Por favor, introduce tu altura en metros:");
+        double altura = scanner.nextDouble();
+        System.out.println("Por favor, introduce tu peso en kilogramos:");
+        double peso = scanner.nextDouble();
+        System.out.println("Altura: " + altura + " m, Peso: " + peso + " kg.");
     }
-
 }
+
+
+
