2b3069b94b52e4f86d67cba7b9d04302a4bf02be
PFBregon <bregon@gmail.com>
2025-03-19 14:51:17 +0100

    IMCCalculator Test created and failed

--- src/test/java/dev/patriciafb/imc/Test/ImcCalculatorTest.java
+++ src/test/java/dev/patriciafb/imc/Test/ImcCalculatorTest.java
@@ -1,4 +1,18 @@
 package dev.patriciafb.imc.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import dev.patriciafb.imc.models.ImcCalculator;
+
 public class ImcCalculatorTest {
+
+private final ImcCalculator calculator = new ImcCalculator();
+
+    @Test
+    void testCalcularIMC() {
+        int peso = 70;
+        double altura = 1.75;
+        double imc = calculator.calcularIMC(peso, altura);
+        assertEquals(22.86, imc);
+    }
 }
